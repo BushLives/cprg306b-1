c03abbae17b1b4275fcf17cd9ec595fa3e9ac362
noahg1238 <noah.greaves@edu.sait.ca>
2026-05-30 16:21:12 -0600

    created app as well as pages for the three conversions.

---
+++ conversion-app/app/mile-kilometre.tsx
@@ -0,0 +1,6 @@
+export default function MileKilometre() {
+  return (
+    <div className="">
+    </div>
+  );
+}
---
+++ conversion-app/app/pound-kilogram.tsx
@@ -0,0 +1,6 @@
+export default function PoundKilogram() {
+  return (
+    <div className="">
+    </div>
+  );
+}
