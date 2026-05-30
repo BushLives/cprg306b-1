b6f24045e4f74910a15fb1413f7b9291cb0083e1
noahg1238 <noah.greaves@edu.sait.ca>
2026-05-30 17:16:11 -0600

    put nav bar in nav tag.

--- conversion-app/app/pounds-kilograms/page.tsx
+++ conversion-app/app/pounds-kilograms/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function PoundsKilograms() {
   return (
     <div className="">
-      <Link href="/pounds-kilograms">pounds-kilograms</Link>
-      <br />
-      <Link href="/miles-kilometres">miles-kilometres</Link>
-      <br />
-      <Link href="/farenheit-celcius">farenheit-celcius</Link>
+      <nav>
+        <Link href="/pounds-kilograms">pounds-kilograms</Link>
+        <br />
+        <Link href="/miles-kilometres">miles-kilometres</Link>
+        <br />
+        <Link href="/farenheit-celcius">farenheit-celcius</Link>
+      </nav>
     </div>
   );
 }
