eb9d1566b8540a1a3152efe82649871ba8d1bf68
noahg1238 <noah.greaves@edu.sait.ca>
2026-05-30 21:51:38 -0600

    added info to pages

---
+++ app/pounds-kilograms/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function PoundsKilograms() {
+  return (
+    <div className="bg-gray-800 font-sans">
+      <nav className="bg-gray-800 shadow-md w-full rounded">
+        <div className="max-w-7xl mx-auto px-4 flex items-center justify-between h-16">Imperial & Metric Converter
+          <div className="hidden md:flex space-x-8">
+            <a href="/pounds-kilograms" className="text-white hover:text-blue-600">pounds-kilograms</a>
+            <a href="/miles-kilometres" className="text-white hover:text-blue-600">miles-kilometres</a>
+            <a href="/farenheit-celcius" className="text-white hover:text-blue-600">farenheit-celcius</a>
+          </div>
+        </div>
+      </nav>
+      
+      <div>
+        <h1>Convert between pounds and kilograms</h1>
+        {/* input box */}
+        <div></div>
+        {/* switch button */}
+        <div></div>
+        {/* output box */}
+        <div></div>
+      </div>
+    </div>
+  );
+}
