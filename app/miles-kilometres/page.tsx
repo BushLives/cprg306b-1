95efdee9e4d2e28824a1bd7af7af381a09d8dc04
BushLives <Kevinl2406@outlook.com>
2026-05-30 22:13:28 -0600

    removing <divs> from places

--- app/miles-kilometres/page.tsx
+++ app/miles-kilometres/page.tsx
@@ -5,23 +5,23 @@ export default function MilesKilometres() {
     <div className="bg-gray-800 font-sans">
       
 
-      <div>
-        <h1>Miles to Kilometres</h1>
-        {/* input box */}
-        <div>
-          <input type="number" id="miles" placeholder="0" className="block min-w-0 bg-gray-700 py-0.5 pl-1 text-white placeholder:text-gray-400 focus:outline"></input>
-          
-          <button type="button" id="convert-button" className="rounded-md px-1 py-0.5 text-sm font-medium text-black shadow-sm hover:bg-gray-700">
-            Convert
-          </button>
-        </div>
-        <div></div>
-        {/* switch button */}
-        <div></div>
-        <p id="resultKM" className="mt-4 text-lg font-semibold">HELP ME</p>
-        {/* output box */}
-        <div></div>
-      </div>
+      
+      <h1>Miles to Kilometres</h1>
+      {/* input box */}
+      
+        <input type="number" id="miles" placeholder="0" className="block min-w-0 bg-gray-700 py-0.5 pl-1 text-white placeholder:text-gray-400 focus:outline"></input>
+        
+        <button type="button" id="convert-button" className="rounded-md px-1 py-0.5 text-sm font-medium text-black shadow-sm hover:bg-gray-700">
+          Convert
+        </button>
+      
+      
+      {/* switch button */}
+      
+      <p id="resultKM" className="mt-4 text-lg font-semibold">HELP ME</p>
+      {/* output box */}
+    
+      
     </div>
   );
 }
