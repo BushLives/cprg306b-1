5f73a3289d7b72b077df11b29486634bd2ef4fd3
noahg1238 <noah.greaves@edu.sait.ca>
2026-05-30 22:11:12 -0600

    Update page.tsx

--- app/pounds-kilograms/page.tsx
+++ app/pounds-kilograms/page.tsx
@@ -16,7 +16,13 @@ export default function PoundsKilograms() {
       <div>
         <h1>Convert between pounds and kilograms</h1>
         {/* input box */}
-        <div></div>
+         <div>
+          <input type="number" id="miles" placeholder="0" className="block min-w-0 bg-gray-700 py-0.5 pl-1 text-white placeholder:text-gray-400 focus:outline"></input>
+          
+          <button type="button" id="convert-button" className="rounded-md px-1 py-0.5 text-sm font-medium text-black shadow-sm hover:bg-gray-700">
+            Convert
+          </button>
+        </div>
         {/* switch button */}
         <div></div>
         {/* output box */}
